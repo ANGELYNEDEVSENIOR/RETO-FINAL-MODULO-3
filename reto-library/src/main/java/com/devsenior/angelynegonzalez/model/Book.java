e471d22ae79378ff420f0f8f561713f2c7a658cc
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-03-23 22:00:20 -0500

    Creacion de clase Book

---
+++ reto-library/src/main/java/com/devsenior/angelynegonzalez/model/Book.java
@@ -0,0 +1,27 @@
+package com.devsenior.angelynegonzalez.model;
+
+public class Book {
+ //estamos definiendo los atributos
+    private String isbn;
+    private String title;
+    private String author;
+ //estamos definiendo el constructor   
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+// estamos definiendo los getters
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
